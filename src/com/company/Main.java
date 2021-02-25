c116d7562918bee68126fde7f33387b0afba3581
Yulia Kozlova <rcoldhan@gmail.com>
2021-02-25 21:55:59 +0300

    final

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * На вход оиждаю строку формата a(операция)b.
+ * При вводе числа double в качестве разделителя ожидаю точку.
+ * Программа завершается при вводе exit.
+ */
 public class Main {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
@@ -13,18 +18,17 @@ public class Main {
         Multiplication multi = new Multiplication();
         Division div = new Division();
 
-        System.out.println("Введи текст в формате 'a (операция) b' и нажми Enter");
+        System.out.println("Введи текст в формате 'a(операция)b' и нажми Enter");
         System.out.println("Для завершения программы введи 'exit'");
-        double a = 0, b = 0;
+        double a, b;
         String operation;
 
         while (true) {
-            operation = "";
             String str = console.nextLine();
             if (str.equals("exit")) {
                 return;
             }
-            String regex = "^\\s*(\\d+(.\\d+)?)\\s*([*+/-])\\s*(\\d+(.\\d+)?)\\s*$";
+            String regex = "^\\s*(-?\\d+(\\.\\d+)?)\\s*([*+/-])\\s*(-?\\d+(\\.\\d+)?)\\s*$";
             Pattern pattern = Pattern.compile(regex);
             Matcher matcher = pattern.matcher(str);
 
@@ -32,25 +36,25 @@ public class Main {
                 a = Double.parseDouble(matcher.group(1));
                 operation = matcher.group(3);
                 b = Double.parseDouble(matcher.group(4));
+
+                switch (operation) {
+                    case "+":
+                        add.execute(a, b);
+                        break;
+                    case "-":
+                        sub.execute(a, b);
+                        break;
+                    case "*":
+                        multi.execute(a, b);
+                        break;
+                    case "/":
+                        div.execute(a, b);
+                        break;
+                    default:
+                        break;
+                }
             } else {
                 System.out.println("Неверный ввод!");
-            }
-
-            switch (operation) {
-                case "+":
-                    add.execute(a, b);
-                    break;
-                case "-":
-                    sub.execute(a, b);
-                    break;
-                case "*":
-                    multi.execute(a, b);
-                    break;
-                case "/":
-                    div.execute(a, b);
-                    break;
-                default:
-                    break;
             }
         }
     }
